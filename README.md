bff914426db3830a6275cc829f5e87ed937a0893
Thiago Nunes Batista <41683571+ThiagoNunesBatista@users.noreply.github.com>
2020-05-15 18:25:17 -0300

    Update Project Name and Readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# reactjs-courses-projects
-Projects Created In ReactJS Courses.
+# GuitiRub - An Alternative Front End For GitHub
+
+## About This Project
+
+This project was built by [Thiago Nunes Batista](https://www.linkedin.com/in/thiagonunesbatista), a Brazilian student in his journey to become a full stack web developer. If you want to take a look in his other projects, just open [this link](https://github.com/ThiagoNunesBatista?tab=repositories).
+
+## What Is This Project About
+
+Create an alternative GitHub Front End using the [GitHub public api](https://developer.github.com/v3) and [ReactJS](https://reactjs.org/).
+
+## How To Setup The Project
+
+1. Enter in the desired project directory
+
+2. Clone the repository
+
+    ```bash
+      git clone https://github.com/ThiagoNunesBatista/GuitiRub.git
+    ```
+
+3. Enter in the project folder and install dependencies
+
+    ```bash
+    cd GuitiRub && npm install
+    ```
+
+4. Start the project
+
+    ```bash
+    npm start
+    ```
+
+## LICENSE
+
+This project is Open Source and is licensed under [MIT license](https://github.com/ThiagoNunesBatista/GuitiRub/blob/master/LICENSE). Feel free to do whatever you want with this code. If you want to contribute to this code, issues and pull requests are very welcome.
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,14 +4,17 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <h1>About This Project</h1>
-      <p>This project was built by Thiago Nunes Batista, a Brazilian student in his journey to become a full stack web developer. If you want to take a look in his other projects, just open <a href='https://github.com/ThiagoNunesBatista?tab=repositories' target='_blank' rel='noopener noreferrer'>this link</a></p>
+      <h1>About The Project</h1>
+      <p>This project was built by <a href='https://www.linkedin.com/in/thiagonunesbatista' target='_blank' rel='noopener noreferrer'>Thiago Nunes Batista</a>, a Brazilian student in his journey to become a full stack web developer. If you want to take a look in his other projects, just open <a href='https://github.com/ThiagoNunesBatista?tab=repositories' target='_blank' rel='noopener noreferrer'>this link</a>.</p>
 
-      <h2>What Is This Project About?</h2>
-      <p>Create similar clones of GitHub features using the <a href='https://developer.github.com/v3/' target='_blank' rel='noopener noreferrer'>GitHub public api</a></p>
+      <h2>What Is The Project About?</h2>
+      <p>Create an alternative GitHub Front End using the <a href='https://developer.github.com/v3' target='_blank' rel='noopener noreferrer'>GitHub public api</a> and <a href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>ReactJS</a></p>
 
-      <h2>Studies Objective</h2>
-      <p>Study and practice Front End development with <a href='https://reactjs.org/' target='_blank' rel='noopener noreferrer'>ReactJS</a></p>
+      <h2>Thiago Nunes Batista Contact Information</h2>
+      <a href='https://github.com/ThiagoNunesBatista' target='_blank' rel='noopener noreferrer'><i className='fab fa-github fa-3x' /></a>
+      <a href='https://www.linkedin.com/in/thiagonunesbatista' target='_blank' rel='noopener noreferrer'><i className='fab fa-linkedin fa-3x' /></a>
+      <a href='https://dev.to/thiagonunesbatista' target='_blank' rel='noopener noreferrer'><i className='fab fa-dev fa-3x' /></a>
+
     </div>
   )
 }
