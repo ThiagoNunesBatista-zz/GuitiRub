9e2e76be7b18286fc3613c1193d4199776d4a812
Thiago Nunes Batista <41683571+ThiagoNunesBatista@users.noreply.github.com>
2020-05-15 10:49:09 -0300

    Add Initial GitHub Finder structure

---
+++ github-finder/src/App.js
@@ -0,0 +1,15 @@
+// External Imports
+import React, { Component } from 'react'
+
+// Internal Imports
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <h1>Hello World From React</h1>
+    )
+  }
+}
+
+export default App
---
+++ github-finder/src/index.js
@@ -0,0 +1,13 @@
+// External Imports
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Internal Imports
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
